3b8be9263711bba00f589ffeaae9417bb2d35d9b
Tookai <tybo.orcel@gmail.com>
2022-05-30 21:28:18 +0200

    disable swipe back action on auth screens

--- navigation/authRoutes.tsx
+++ navigation/authRoutes.tsx
@@ -17,6 +17,7 @@ const AuthNavigation = () => {
       screenOptions={{
         headerShown: false,
         animationTypeForReplace: "push",
+        gestureEnabled: false,
       }}
     >
       {/*   ----- -------  -----   */}
